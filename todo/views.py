73980faa49af5fa9cc3a14aa491c06341c84809d
sidx <sidharth.xtb@gmail.com>
2015-01-19 23:26:13 +0530

    Adding Views

--- todo/views.py
+++ todo/views.py
@@ -6,3 +6,22 @@ from todo.models import Item
 def list_view(request):
 	items = Item.objects.all()
 	return render_to_response('list_view.html',{'items'.items})
+
+def todo_add(request):
+	if request.method == "POST":
+		item_name = request.POST['item']
+		new_item = Item()
+		new_item.title = item_name
+		new_item.description = description
+		new_item.due_date = due_date
+		new_item.priority = priority
+		new_item.status = status
+		new_item.save()
+		return redirect('/')
+
+
+def todo_del(request, item_id):
+	item = Item.objects.get(id=item_id)
+	task.delete()
+	return redirect('/')
+	
